baa70f4823e562e49174874e51a9af7304d6d0f2
Chaichana Lohasaptawee <twinsamson@gmail.com>
2022-04-14 13:27:17 +0700

    finish lab1

--- kern/main.c
+++ kern/main.c
@@ -9,13 +9,15 @@ main()
     /*
      * Before doing anything else, we need to ensure that all
      * static/global variables start out zero.
+     * The edata and end are defined in kern/linker.ld.
      */
-
     extern char edata[], end[];
 
     /* TODO: Use `memset` to clear the BSS section of our program. */
+    memset(edata, 0, end-edata);
 
     /* TODO: Use `cprintf` to print "hello, world\n" */
+    cprintf("Hello, world!\n");
 
     while (1) ;
 }
